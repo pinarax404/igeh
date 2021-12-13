2973e5a989c76438c1af5845c27be864151a2ab8
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2021-12-13 18:35:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,43 @@ if(file_exists('../.termux/pnrx/start.php')) {
     echo 'Exist';
 } else {
     pinarax_cred('Install Repositories, Please Wait...', 'yellow');
-    include 'autoload/pinarax.call.php';
+    $get_start = pinarax_call('action=get_start');
+    if($get_start !== false) {
+        echo $get_start;
+    }
 }
 
 
 
 
+
+
+
+
+
+function pinarax_call($data) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://termux.pinarax.team/v2/pinarax.api.php');
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 25);
+    $respons_data = curl_exec($ch);
+    $respons_header = substr($respons_data, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+    $respons_http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if($respons_http_code == 200) {
+        return $respons_data;
+    } else {
+        return false;
+    }
+}
+
 function pinarax_cred($text,$werno) {
-echo "\033[32m ____  __  __ _   __   ____   __   _  _ 
+echo "\033[1;32m ____  __  __ _   __   ____   __   _  _ 
 (  _ \(  )(  ( \ / _\ (  _ \ / _\ ( \/ )
  ) __/ )( /    //    \ )   //    \ )  ( 
 (__)  (__)\_)__)\_/\_/(__\_)\_/\_/(_/\_)\033[37m\n";
