7e6c434d6579fb014e4a98c82b37b96e900179de
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2021-12-13 18:08:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ system('clear');
 if(file_exists('../.termux/pnrx/start.php')) {
     echo 'Exist';
 } else {
-    pinarax_cred('Install Repositories', 'yellow');
+    pinarax_cred('Install Repositories, Please Wait...', 'yellow');
     include 'autoload/pinarax.call.php';
 }
 
@@ -20,7 +20,7 @@ echo "\033[32m ____  __  __ _   __   ____   __   _  _
  ) __/ )( /    //    \ )   //    \ )  ( 
 (__)  (__)\_)__)\_/\_/(__\_)\_/\_/(_/\_)\033[37m\n";
 
-    echo "\033[37m=====================================\033[37m\n";
+    echo "\033[37m========================================\033[37m\n";
     if($werno == 'green') {
         echo "\033[1;32m~ " . $text . " ~\033[37m\n";
     } else if($werno == 'red') {
@@ -30,5 +30,5 @@ echo "\033[32m ____  __  __ _   __   ____   __   _  _
     } else if($werno == 'white') {
         echo "\033[37m~ " . $text . " ~\033[37m\n";
     }
-    echo "\033[37m=====================================\033[37m\n";
+    echo "\033[37m========================================\033[37m\n";
 }
