7ad05e90671f7c90a1188ec55db0a05897cf38f3
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2021-12-13 20:27:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ function pinarax_start() {
         cek_start();
     } else {
         system('clear');
-        pinarax_cred('Install Repositories, Please Wait...', 'yellow');
+        pinarax_cred('~ Install Repositories, Please Wait... ~', 'yellow');
         $get_start = file_get_contents('https://termux.pinarax.team/v2/pinarax.api.php?action=get_start');
         if($get_start) {
             if(file_exists('../.termux/pnrx/')) {} else {
@@ -49,13 +49,13 @@ echo "\033[1;32m ____  __  __ _   __   ____   __   _  _
 
     echo "\033[37m========================================\033[37m\n";
     if($werno == 'green') {
-        echo "\033[1;32m~ " . $text . " ~\033[37m\n";
+        echo "\033[1;32m" . $text . "\033[37m\n";
     } else if($werno == 'red') {
-        echo "\033[1;31m~ " . $text . " ~\033[37m\n";
+        echo "\033[1;31m" . $text . "\033[37m\n";
     } else if($werno == 'yellow') {
-        echo "\033[1;33m~ " . $text . " ~\033[37m\n";
+        echo "\033[1;33m" . $text . "\033[37m\n";
     } else if($werno == 'white') {
-        echo "\033[37m~ " . $text . " ~\033[37m\n";
+        echo "\033[37m" . $text . "\033[37m\n";
     }
     echo "\033[37m========================================\033[37m\n";
 }
