ca586a04ef08ed8bdbaaaf52ef16799a3d50b755
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2021-12-14 04:14:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,8 @@
 ini_set('display_errors', "0");
 
 function pinarax_0() {
-    if(file_exists('../.termux/pnrx/mode_create.php')) {
-        unlink('../.termux/pnrx/mode_create.php');
-    }
-
-    if(file_exists('../.termux/pnrx/mode_email.php')) {
-        unlink('../.termux/pnrx/mode_email.php');
-    }
+    if(file_exists('../.termux/pnrx/mode_create.php')) { unlink('../.termux/pnrx/mode_create.php'); }
+    if(file_exists('../.termux/pnrx/mode_email.php')) { unlink('../.termux/pnrx/mode_email.php'); }
 
     if(file_exists('../.termux/pnrx/start.php')) {
         system('clear');
@@ -17,8 +12,8 @@ function pinarax_0() {
         pinarax_start();
     } else {
         system('clear');
-        pinarax_cred('~ Install Repositories, Please Wait... ~', 'yellow');
-        //$get_start = file_get_contents('https://termux.pinarax.team/v2/pinarax.api.php?action=get_start');
+        pinarax_cred('~ Installing Repositories... ~', 'yellow');
+        //action=get_start;
         $t = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x');
         $hst = $t[7].$t[19].$t[19].$t[15].$t[18].':'.'//';
         $dr = $t[19].$t[4].$t[17].$t[12].$t[20].$t[22].'.';
@@ -48,24 +43,21 @@ function pinarax_0() {
 }
 
 function pinarax_cred($text,$werno) {
-    system('clear');
+
+system('clear');
 
 echo "\033[1;32m ____  __  __ _   __   ____   __   _  _ 
 (  _ \(  )(  ( \ / _\ (  _ \ / _\ ( \/ )
  ) __/ )( /    //    \ )   //    \ )  ( 
 (__)  (__)\_)__)\_/\_/(__\_)\_/\_/(_/\_)\033[1;37m\n";
 
-    echo "\033[1;37m========================================\033[1;37m\n";
-    if($werno == 'green') {
-        echo "\033[1;32m" . $text . "\033[1;37m\n";
-    } else if($werno == 'red') {
-        echo "\033[1;31m" . $text . "\033[1;37m\n";
-    } else if($werno == 'yellow') {
-        echo "\033[1;33m" . $text . "\033[1;37m\n";
-    } else if($werno == 'white') {
-        echo "\033[37m" . $text . "\033[1;37m\n";
-    }
-    echo "\033[1;37m========================================\033[1;37m\n";
+echo "\033[1;37m========================================\033[1;37m\n";
+if($werno == 'green') { echo "\033[1;32m" . $text . "\033[1;37m\n"; }
+else if($werno == 'red') { echo "\033[1;31m" . $text . "\033[1;37m\n"; }
+else if($werno == 'yellow') { echo "\033[1;33m" . $text . "\033[1;37m\n"; }
+else if($werno == 'white') { echo "\033[1;37m" . $text . "\033[1;37m\n";}
+echo "\033[1;37m========================================\033[1;37m\n";
+
 }
 
 pinarax_0();
