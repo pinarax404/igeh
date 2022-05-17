c2fad9ff627d5e4c4eba64c00558f36c546df8e4
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-17 07:16:24 +0700

    Update termux.php

--- termux.php
+++ termux.php
@@ -162,7 +162,7 @@ function pinarax_curl_ig($url, $data, $httpheader, $header, $csrftoken, $save_co
 			'x-ig-www-claim: 0',
 			'x-instagram-ajax: 9bcc5b5208c5',
             'x-csrftoken: ' . $csrftoken,
-			'user-agent: Mozilla/5.0 (Linux; Android 9; SM-A102U Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Instagram 155.0.0.37.107 Android (28/9; 320dpi; 720x1468; samsung; SM-A102U; a10e; exynos7885; en_US; 239490550)'
+			'user-agent: Mozilla/5.0 (PlayBook; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML, like Gecko) Version/7.2.1.0 Safari/536.2+'
         ));
     }
     if($data) {
