93b1171e004ffc5a9e43baf156db81d5a262edb2
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-17 12:25:54 +0700

    Update termux.php

--- termux.php
+++ termux.php
@@ -83,7 +83,7 @@ function pinarax_start_create() {
                     if($submit_code !== false  && strpos($submit_code, 'signup_code') !== false) {
                         $res_submit_code = json_decode($submit_code, true);
                         $signup_code = $res_submit_code['signup_code'];
-                        $p_create_ajax = 'enc_password=%23PWD_INSTAGRAM_BROWSER%3A10%3A1652744970%3A'.$res_password.'&email='.$res_email_id.'&username='.$res_username.'&first_name='.$res_name.'&client_id='.$res_ig_mid.'&seamless_login_enabled=1&opt_into_one_tap=false&tos_version=eu&force_sign_up_code='.$signup_code;
+                        $p_create_ajax = 'enc_password=#PWD_INSTAGRAM_BROWSER:0:'.time().':'.$res_password.'&email='.$res_email_id.'&username='.$res_username.'&first_name='.$res_name.'&client_id='.$res_ig_mid.'&seamless_login_enabled=1&opt_into_one_tap=false&tos_version=eu&force_sign_up_code='.$signup_code;
 						$create_ajax = pinarax_curl_ig('https://www.instagram.com/accounts/web_create_ajax/', $p_create_ajax, true, false, $res_ig_csrftoken, false, 'respons_data');
                         if($create_ajax){
                             $p_login_ajax = 'enc_password=#PWD_INSTAGRAM_BROWSER:0:'.time().':'.$res_password.'&username='.$res_username.'&queryParams=%7B%7D&optIntoOneTap=false&stopDeletionNonce=&trustedDeviceRecords=%7B%7D';
@@ -154,15 +154,12 @@ function pinarax_curl_ig($url, $data, $httpheader, $header, $csrftoken, $save_co
     curl_setopt($ch, CURLOPT_URL, $url);
     if($httpheader) {
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'content-type: application/x-www-form-urlencoded',
-			'origin: https://www.instagram.com',
-			'referer: https://www.instagram.com',
-			'x-asbd-id: 198387',
-			'x-ig-app-id: 936619743392459',
-			'x-ig-www-claim: 0',
-			'x-instagram-ajax: 9bcc5b5208c5',
-            'x-csrftoken: ' . $csrftoken,
-			'user-agent: Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
+			'Host: www.instagram.com',
+			'X-CSRFToken: ' . $csrftoken,
+			'X-Instagram-AJAX: 1',
+			'X-Requested-With: XMLHttpRequest',
+			'Referer: https://www.instagram.com/',
+			'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS '.rand(000, 999).' like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 [FBAN/FBIOS;FBDV/iPhone9,1;FBMD/iPhone;FBSN/iOS;FBSV/13.5.1;FBSS/2;FBID/phone;FBLC/en_US;FBOP/5]'
         ));
     }
     if($data) {
